217fd3ab0527437982f75f2abd4b85dd348d3679
ClaudinhoPedrotti <claudio.brito9@etec.sp.gov.br>
2019-03-14 22:14:58 -0300

    Aula 14.03.2019

---
+++ src/com/agenda/model/Pessoa.java
@@ -0,0 +1,38 @@
+package com.agenda.model;
+
+public class Pessoa {
+	
+		public String nome;
+		String endereco;
+		String email;
+		String senha;
+
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getEndereco() {
+			return endereco;
+		}
+		public void setEndereco(String endereco) {
+			this.endereco = endereco;
+		}
+	
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getSenha() {
+			return senha;
+		}
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+			
+	
+}
--- src/com/agenda/servlet/CadastroUsuarioServlet.java
+++ src/com/agenda/servlet/CadastroUsuarioServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.agenda.model.Pessoa;
+
 public class CadastroUsuarioServlet  extends HttpServlet{
 
 	/**
@@ -16,9 +18,32 @@ public class CadastroUsuarioServlet  extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	 /**	
 		request.getParameter("exampleInputEmail1");
 		System.out.println(request.getParameter("exampleInputEmail1"));
+		request.getParameter("exampleInputPassword1");
+		System.out.println(request.getParameter("exampleInputPassword1"));
+		
+		
+		/** Calculadora 
+		 */
+		/**
+		String numero1, numero2, resultado;
+		
+		numero1 = request.getParameter("numero1");
+		numero2 = request.getParameter("numero2");
+		resultado = numero1 + numero2;
+		System.out.println(resultado);
+		
+		
+		/** Calculadora 
+		 */
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome(request.getParameter("nome"));
+		pessoa.setEndereco(request.getParameter("endereco"));
+		pessoa.setEmail(request.getParameter("email"));
+		pessoa.setSenha(request.getParameter("senha"));
 		
 		
 	}
