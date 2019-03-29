2f657800599b895efa1ab1b31f4c3756bb673f06
ClaudinhoPedrotti <claudio.brito9@etec.sp.gov.br>
2019-03-28 22:09:17 -0300

    PessoaDao

---
+++ src/com/agenda/util/ConnectionFactory.java
@@ -0,0 +1,18 @@
+package com.agenda.util;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+public class ConnectionFactory {
+
+	public Connection getConnection() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return (Connection) DriverManager.getConnection("jbdc:mysql://localhost/agendadb", "root", "");
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+	}
+}
